56d2e6c0b6fd9aa89f627147cad2f32192dcfecc
zhuanzhaobuchixiangcai <zhuanzhaobuchixiangcai@users.noreply.github.com>
2026-06-04 19:28:59 +0800

    init: 极简单词本 PWA

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'vocab-v1';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) =>
+      cached || fetch(e.request).then((resp) => {
+        if (resp.ok && e.request.url.startsWith(self.location.origin)) {
+          const clone = resp.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, clone));
+        }
+        return resp;
+      })
+    )
+  );
+});
