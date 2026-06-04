b77fcd797b53c2d654cba4938ba06ba8dd641906
zhuanzhaobuchixiangcai <zhuanzhaobuchixiangcai@users.noreply.github.com>
2026-06-04 20:26:21 +0800

    fix: add PNG icons for PWA install compatibility

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'vocab-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+const CACHE = 'vocab-v2';
+const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
